4042a7b3904232c22f7f0ec35f38f3892e2df3c4
MrSuIR <MrSuIR>
2025-05-05 19:55:36 +0300

    Initial commit: Added FastAPI booking API

---
+++ src/migrations/versions/2025_05_04_2227-7948253f6b53_add_rooms.py
@@ -0,0 +1,31 @@
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "7948253f6b53"
+down_revision: Union[str, None] = "0a6ed15f1a6c"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rooms")
